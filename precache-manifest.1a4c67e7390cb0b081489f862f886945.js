cd3688329cd0a4c4f298998f3025897c68a9a698
Sergey Boiarskii <sergvinn2008@gmail.com>
2021-05-18 21:17:48 +0300

    Updates

---
+++ precache-manifest.1a4c67e7390cb0b081489f862f886945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7d4e02fd6fb818fd5c1a6b408dada5",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "a25343392acf5b6a2728",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.04d8a96a.chunk.css"
+  },
+  {
+    "revision": "59168d4a4b8b6b2e19c2",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.fbcdb93d.chunk.js"
+  },
+  {
+    "revision": "a25343392acf5b6a2728",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.311b547f.chunk.js"
+  },
+  {
+    "revision": "3dc2d4bc992ea5be91b7",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.7345331c.js"
+  }
+]);
